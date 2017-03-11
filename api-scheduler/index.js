fe0819f8b2bc0bb689be093a3a05c2d1db5695f4
Botond Fekete <fekbot@gmail.com>
2017-03-11 19:16:15 +0200

    Removed code repetitions

--- api-scheduler/index.js
+++ api-scheduler/index.js
@@ -1,30 +1,11 @@
-const url = require("url")
-
 const config = require("./config")
+const bootstrapApi = require("../lib/api")
 
-const bodyParser = require("body-parser")
-const cors = require("cors")
+bootstrapApi(config, api => {
+  
+  api.post("schedule", (req, res) => {
+    console.log("SCHEDULE TASK")
+    res.send(200)
+  })
 
-const app = require("express")()
-
-const port = url.parse(config.server.url).port || 8000
-
-app.use(bodyParser.json())
-app.use(cors({
-  origin: (origin, cb) => {
-    if (!config.server.cors || config.server.cors === "*" || config.server.cors.includes(origin)) {
-      return cb(null, true)
-    }
-    cb(new Error("Not allowed"), false)
-  }
-}))
-
-app.post("schedule", function (req, res) {
-  console.log("SCHEDULE TASK")
-  res.send(200)
 })
-
-app.listen(port, () => {
-  console.log(`${config.server.name} started and listening on port ${port}`)
-})
-
