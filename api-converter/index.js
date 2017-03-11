46e4cc8cdb186a9afa7adb7cac7517ebf78373f8
Botond Fekete <fekbot@gmail.com>
2017-03-12 01:34:09 +0200

    Added progress tracking to conversion and writing it to the schedule response stream

--- api-converter/index.js
+++ api-converter/index.js
@@ -1,21 +1,35 @@
+"use strict"
+
 const config = require("./config")
 const bootstrapApi = require("../lib/api")
 
 const PROCESSTIME = {
-  HTML: 100,
+  HTML: 10000,
   PDF: 10,
 }
 
-bootstrapApi(config, api => {
+const STEPS = 100
 
-  api.post("/to-html", (req, res) => {
-    console.log("TO HTML")
-    res.status(200).send()
-  })
+const routeSpec = {
+  "/to-html": {
+    post: (req, res) => {
+      let track = 0
+      const progress = setInterval(() => {
+        res.write("" + track++)
+        console.log(track)
+        if (track === STEPS) {
+          clearInterval(progress)
+          res.end()
+        }
+      }, PROCESSTIME.HTML / STEPS)
+    },
+  },
+  "/to-pdf": {
+    post: (req, res) => {
+      console.log("TO PDF")
+      res.status(200).send()
+    },
+  },
+}
 
-  api.post("/to-pdf", (req, res) => {
-    console.log("TO PDF")
-    res.status(200).send()
-  })
-
-})
+bootstrapApi(config, routeSpec, () => {})
--- api-scheduler/index.js
+++ api-scheduler/index.js
@@ -1,3 +1,7 @@
+"use strict"
+
+let io
+
 const fetch = require("node-fetch")
 const bodyParser = require("body-parser")
 
@@ -8,24 +12,30 @@ const serviceMap = {
   "converter": require("../api-converter/config").server.url,
 }
 
-bootstrapApi(config, api => {
+const routeSpec = {
+  "/schedule": {
+    post: (req, res) => {
+      const {service, task, method, data} = req.body
+      const url = serviceMap[service]
 
+      if (!url) return res.status(400).send("Requested service does not exist")
+
+      fetch(`${url}/${task}`, {method, body: data})
+        .then(result => {
+          res.status(200).send("OKAY!")
+          result.body.on("data", (data) => console.log(data.toString()))
+        })
+        .catch(err => {
+          console.log(err)
+          res.status(500).send(`There was an error connecting to service '${service}'`)
+        })
+      
+      return null
+    },
+  },
+}
+
+bootstrapApi(config, routeSpec, api => {
+  io = require("socket.io").listen(require("http").createServer(api))
   api.use(bodyParser.json())
-  
-  api.post("/schedule", (req, res) => {
-    const {service, task, method, data} = req.body
-    const url = serviceMap[service]
-
-    if (!url) return res.status(400).send("Requested service does not exist")
-
-    fetch(`${url}/${task}`, {method, body: data})
-      .then(result => {
-        console.log(result)
-        res.status(200).send("OKAY!")
-      })
-      .catch(() => res.status(500).send(`There was an error connecting to service ${service}`))
-    
-    return null
-  })
-
 })
