a27188b7a3919c2de9f5db43f3a8cddb72c471da
Botond Fekete <fekbot@gmail.com>
2017-03-18 11:06:43 +0200

    Added build steps to compile css

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
 const gulp = require('gulp')
+
 const browserify = require('browserify')
 const source = require('vinyl-source-stream')
+
+const nano = require('cssnano')
+const autoprefixer = require('autoprefixer')
+const sass = require('gulp-sass')
+const postcss = require('gulp-postcss')
+
+const sourcemaps = require('gulp-sourcemaps')
 
 gulp.task('bundle', () => 
   browserify({
@@ -8,5 +16,18 @@ gulp.task('bundle', () =>
   })
   .bundle()
   .pipe(source('bundle.js'))
+  .pipe(gulp.dest('app-ui/dist'))
+)
+
+gulp.task('styles', () => 
+  gulp.src('./app-ui/src/scss/styles.scss')
+  .pipe(sourcemaps.init())
+  .pipe(sass({
+    includePaths: [
+      __dirname + '/node_modules/normalize-scss/sass',
+    ],
+  }))  
+  .pipe(postcss([autoprefixer(), nano()]))
+  .pipe(sourcemaps.write('.'))
   .pipe(gulp.dest('app-ui/dist'))
 )
